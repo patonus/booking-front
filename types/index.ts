59bf8dc3cdf008482b3ed5e46bb569f448d09c5a
Patryk Wojtowicz <pwojtowicz96@gmail.com>
2024-05-16 13:12:03 +0200

    Start implementing reservation form

--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,11 @@ export type ApiPage<Data extends Record<string, unknown>> = {
 	data: Data[]
 	last_page: number
 }
+
+export type ReservationData = {
+	start_date: string
+	end_date: string
+	count: number
+	// Kind of a hack, to allow type-safe handling of API errors not related to a given field
+	form?: never
+}
